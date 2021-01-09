5178f66d12aad96e99e421c15307e7a89d302940
Ludwig Dickmanns <ludwig.dickmanns@outlook.de>
2021-01-09 09:52:52 +0100

    The app now displays two preconfigured images next to each other.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,42 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import jdk.jshell.execution.Util
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 
 fun main() {
     Window(
-        icon = getWindowIcon()
+        title = "Would you rather?",
+        size = IntSize(1000, 500),
+        icon = getWindowIcon(),
     ) {
-        val image = imageResource("logo.png")
-        Image(
-            bitmap = image,
-            modifier = Modifier.fillMaxSize()
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    bitmap = imageResource("logo.png"),
+                    modifier = Modifier.preferredHeight(500.dp).preferredWidth(500.dp),
+                    alignment = Alignment.Center
+                )
+                Image(
+                    bitmap = imageResource("logo_mini.png"),
+                    modifier = Modifier.preferredHeight(500.dp).preferredWidth(500.dp),
+                    alignment = Alignment.Center
+                )
+            }
+        }
     }
 }
 
